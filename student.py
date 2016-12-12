3d254a1753f6e33d70a1df279ef1319b481f83dc
none <none>
2016-12-12 19:47:53 +0200

    Made a class to describe a student

---
+++ student.py
@@ -0,0 +1,16 @@
+class Student
+
+    def __init__(name,hometown,age,height,favorite ice creame flavor):
+
+        self.name=name
+        self.hometown=hometown
+        self.age=age
+        self.height=height
+        self.ice creame flavor=ice creame flavor
+
+    def print_all(self):
+        print(self.name+"from"+self.hometown+self.age+"years old"+self.height+"meter"+"like"+self.ice creame flavor+"ice-creame")
+    
+    def get_giraffa_gap(self):
+        return 500-self.height
+        
